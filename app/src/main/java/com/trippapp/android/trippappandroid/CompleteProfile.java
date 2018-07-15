86b6d9892b99b9c7b96c66dbdfac0082a24a0b26
mamad <m.farrokh.1996@gmail.com>
2018-07-15 15:27:51 +0430

    compelete gallary and camera

--- app/src/main/java/com/trippapp/android/trippappandroid/CompleteProfile.java
+++ app/src/main/java/com/trippapp/android/trippappandroid/CompleteProfile.java
@@ -1,22 +1,50 @@
 package com.trippapp.android.trippappandroid;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Environment;
+import android.os.PowerManager;
+import android.provider.ContactsContract;
+import android.provider.MediaStore;
+import android.support.annotation.NonNull;
+import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.ImageButton;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.widget.Toast;
 
-public class CompleteProfile extends AppCompatActivity {
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+
+public class CompleteProfile extends AppCompatActivity implements View.OnClickListener {
+
+    public static final int IMAGE_GALLERY_REQUEST = 20;
+    public static final int CAMERA_REQUEST_CODE = 228;
+    public static final int CAMERA_PERMISSION_REQUEST_CODE = 4192;
 
     TextView username;
-    ImageButton picProfile;
+    ImageView picProfile;
+    Button camera, gallary;
 
     private void initView() {
         picProfile = findViewById(R.id.iv_img_complete_profile);
         username = findViewById(R.id.tv_username_complete_profile);
+        camera = findViewById(R.id.bt_camera_complete_profile);
+        gallary = findViewById(R.id.bt_gallery_complete_profile);
 
     }
 
@@ -25,7 +53,10 @@ public class CompleteProfile extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_complete_profile);
+
         initView();
+
+
     }
 
     @Override
@@ -35,10 +66,121 @@ public class CompleteProfile extends AppCompatActivity {
         item.setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
             @Override
             public boolean onMenuItemClick(MenuItem item) {
-                startActivity(new Intent(CompleteProfile.this,MainActivity.class));
+                startActivity(new Intent(CompleteProfile.this, MainActivity.class));
                 return false;
             }
         });
         return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == camera.getId()) {
+
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {  //min sdk needed is 23
+                if (checkSelfPermission(Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED && checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
+                    invokeCamera();
+                } else {
+                    String[] permissionRequest = {Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE};
+                    requestPermissions(permissionRequest, CAMERA_PERMISSION_REQUEST_CODE);
+                }
+            }
+
+        } else if (v.getId() == gallary.getId()) {
+            // invoke the image gallery using an implict intent.
+            Intent photoPickerIntent = new Intent(Intent.ACTION_PICK);
+
+            // where do we want to find the data?
+            File pictureDirectory = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
+            String pictureDirectoryPath = pictureDirectory.getPath();
+
+            // finally, get a URI representation
+            Uri data = Uri.parse(pictureDirectoryPath);
+
+            // set the data and type.  Get all image types.
+            photoPickerIntent.setDataAndType(data, "image/*");
+
+            // we will invoke this activity, and get something back from it.
+            startActivityForResult(photoPickerIntent, IMAGE_GALLERY_REQUEST);
+
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == CAMERA_PERMISSION_REQUEST_CODE) {
+            // we have heard back from our request for camera and write external storage.
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED && grantResults[1] == PackageManager.PERMISSION_GRANTED) {
+                invokeCamera();
+            } else {
+                Toast.makeText(this, "Can not open the camera", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    private void invokeCamera() {
+        //get a file refrence
+        Uri pictureUri = FileProvider.getUriForFile(this, getApplicationContext().getPackageName() + ".provide", createImagefile());
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
+        //tell the camera where to save the image
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, pictureUri);
+
+        //tell camera request write permission
+        intent.setFlags(Intent.FLAG_GRANT_WRITE_URI_PERMISSION);
+
+        startActivityForResult(intent, CAMERA_REQUEST_CODE);
+    }
+
+    private File createImagefile() {
+        // The public picture directory
+        File pictureDirectory = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
+
+        //TimeStamp makes unic name
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyyMMdd_HHmmss");
+        String timeStamp = simpleDateFormat.format(new Date());
+
+        // put together the directory and the timestamp to make a unique image location.
+        File imageFile = new File(pictureDirectory + "picture" + timeStamp + ".jpg");
+
+        return imageFile;
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (resultCode == RESULT_OK) {
+            if (requestCode == CAMERA_REQUEST_CODE) {
+                Toast.makeText(this, "Image Saved.", Toast.LENGTH_LONG).show();
+            }
+            // if we are here, everything processed successfully.
+            if (requestCode == IMAGE_GALLERY_REQUEST) {
+                // if we are here, we are hearing back from the image gallery.
+
+                // the address of the image on the SD Card.
+                Uri imageUri = data.getData();
+
+                // declare a stream to read the image data from the SD Card.
+                InputStream inputStream;
+
+                // we are getting an input stream, based on the URI of the image.
+                try {
+                    inputStream = getContentResolver().openInputStream(imageUri);
+
+                    // get a bitmap from the stream.
+                    Bitmap image = BitmapFactory.decodeStream(inputStream);
+
+
+                    // show the image to the user
+                    picProfile.setImageBitmap(image);
+
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                    // show a message to the user indictating that the image is unavailable.
+                    Toast.makeText(this, "Unable to open image", Toast.LENGTH_LONG).show();
+                }
+            }
+        }
     }
 }
